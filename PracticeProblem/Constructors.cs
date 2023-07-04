819866ce16473b43ae1a1a83e58d2fca492e4c06
“riteshwaykole21” <riteshwaykole21@gmail.com>
2023-07-04 15:22:27 +0530

    [Ritesh]add : Use Of Constructor

---
+++ PracticeProblem/Constructors.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticeProblem
+{
+    public class Constructors
+    {
+        public int a;
+        public int b;   
+        // This is Un Parameterrised Constructor
+        public Constructors()
+        {
+            Console.WriteLine("This Is Parameter Less Constructor");
+        }
+        // This Is Parameter rised Constructor
+        public Constructors(int a ,int b)
+        {
+            int Sum = a + b;
+            Console.WriteLine(Sum);
+        }
+    }
+}
---
+++ PracticeProblem/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticeProblem
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("1 : Constructor Demo");
+            Console.WriteLine("Enter A Option");
+            int Option = Convert.ToInt32(Console.ReadLine());
+            switch(Option)
+            {
+                case 1:
+                    new Constructors();
+                    new Constructors(10,20);
+                    break;
+            }
+            Console.ReadLine();
+        }
+    }
+}
